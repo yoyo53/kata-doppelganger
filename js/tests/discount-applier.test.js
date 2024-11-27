92030d0572fa18edf94546041061b471ad9dff67
Yohan Villiers <94387950+yoyo53@users.noreply.github.com>
2024-11-27 13:51:20 +0100

    fix discount applier test

--- js/tests/discount-applier.test.js
+++ js/tests/discount-applier.test.js
@@ -1,9 +1,34 @@
-test('apply v1', () => {
-  // TODO: write a test that fails due to the bug in
-  // DiscountApplier.applyV1
-})
+const DiscountApplier = require("../discount-applier");
 
-test('apply v2', () => {
-  // TODO: write a test that fails due to the bug in
-  // DiscountApplier.applyV2
-})
+class Notifier {
+  constructor() {
+    this.calls = [];
+  }
+  notify(user, message) {
+    this.calls.push({ user, message });
+  }
+}
+
+test("apply v1", () => {
+  const notifier = new Notifier();
+  const discountApplier = new DiscountApplier(notifier);
+  const users = ["user1", "user2", "user3"];
+  const discount = 10;
+  const message = `You've got a new discount of ${discount}%`;
+
+  discountApplier.applyV1(discount, users);
+
+  expect(notifier.calls).toEqual(users.map((user) => ({ user, message })));
+});
+
+test("apply v2", () => {
+  const notifier = new Notifier();
+  const discountApplier = new DiscountApplier(notifier);
+  const users = ["user1", "user2", "user3"];
+  const discount = 10;
+  const message = `You've got a new discount of ${discount}%`;
+
+  discountApplier.applyV2(discount, users);
+
+  expect(notifier.calls).toEqual(users.map((user) => ({ user, message })));
+});
